30f93e6d42e45477db3175a60eb9cf3e0a04df72
Michael Clemons <Mclemons@tql.com>
2024-06-24 11:24:44 -0400

    Add project files.

---
+++ EF-Tutorial/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace EF_Tutorial
+{
+    public class AppDbContext : DbContext //adds in the EntityFrameworkCore
+    {
+        public  DbSet<User> Users { get; set; } //tells entity framework what it can access
+        
+
+        public AppDbContext()//default constructor
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) //overides one of the methods in the base class (DbContext).
+                                                                                      //protected is a access modifer.
+                                                                                      //protected says its private for all other classes except for the base class
+                                                                                      //OnConfiguring is a method within DbContext
+        {
+            var connectionString = "Server = localhost\\sqlexpress01;database=tqlprsdb;trusted_connection=true;trustServerCertificate=true;";
+            
+            optionsBuilder.UseSqlServer(connectionString); //passes connection to Entity FrameWork
+        }
+    }
+}
---
+++ EF-Tutorial/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace EF_Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new AppDbContext(); //creates an instance of AppDbContext
+
+            var users = context.Users.ToList(); //gets all of the info from users and saves it to a list
+
+            foreach (var item in users)
+            {
+                Console.WriteLine($"Name: {item.Firstname} {item.Lastname}");
+            }
+        }
+    }
+}
